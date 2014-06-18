cfc35709850912c9050ebffde9a5cac766f2ae44
Ingar Abrahamsen <ingar@abrahams1.com>
2014-06-18 17:53:20 +0200

    Index per day.

--- src/main/java/com/github/ingarabr/mi/EsWriter.java
+++ src/main/java/com/github/ingarabr/mi/EsWriter.java
@@ -1,6 +1,10 @@
 package com.github.ingarabr.mi;
 
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.joda.time.DateTime;
+import org.elasticsearch.common.joda.time.DateTimeZone;
+import org.elasticsearch.common.joda.time.format.DateTimeFormat;
+import org.elasticsearch.common.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +14,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class EsWriter implements Runnable {
 
     private static final Logger logger = LoggerFactory.getLogger(EsWriter.class);
+    private static final DateTimeFormatter DTF = DateTimeFormat.forPattern("yyyy.MM.dd");
 
     private final Client esClient;
     private final BlockingQueue<String> writeQueue = new LinkedBlockingQueue<String>();
@@ -26,7 +31,8 @@ public class EsWriter implements Runnable {
                 String metric = writeQueue.take();
                 log();
                 if (metric != null) {
-                    esClient.prepareIndex("metrics", "metric").setSource(metric).get();
+                    String indexName = "metrics-" + DateTime.now(DateTimeZone.UTC).toString(DTF);
+                    esClient.prepareIndex(indexName, "metric").setSource(metric).get();
                 }
             }
         } catch (InterruptedException e) {
